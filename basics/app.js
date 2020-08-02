8affe8fd47cd2cb75728fbce16fe2909611029f2
Laslaa Mohammed <mohamed.laslaa@gmail.com>
2020-08-02 14:18:44 +0200

    basics

---
+++ basics/app.js
@@ -0,0 +1,11 @@
+var userInput;
+var userName;
+userInput = 5;
+userInput = 'Hello';
+if (typeof userInput === 'string')
+    userName = userInput;
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+}
+var result = generateError('An error occured !', 500);
+console.log(result);
