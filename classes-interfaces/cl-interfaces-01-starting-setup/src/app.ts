7194ba22a35ea07fbbc4bacc8aaf32fb0a362129
Laslaa Mohammed <mohamed.laslaa@gmail.com>
2020-08-11 22:15:34 +0200

    classes & interfaces

--- classes-interfaces/cl-interfaces-01-starting-setup/src/app.ts
+++ classes-interfaces/cl-interfaces-01-starting-setup/src/app.ts
@@ -1,25 +1,45 @@
-interface Greetable {
-  name: string;
+// type AddFn = (a: number, b:number) => number;
+interface AddFn {
+  (a: number, b: number): number;
+}
 
+let Add: AddFn;
+
+Add = (a: number, b: number) => {
+  return a + b;
+};
+
+interface Named {
+  readonly name?: string;
+  outputName?: string;
+}
+
+interface Greetable extends Named {
   greet(phrase: string): void;
 }
 
 class Person implements Greetable {
-  name: string;
+  name?: string;
   age = 30;
 
-  constructor(n: string) {
-    this.name = n;
+  constructor(n?: string) {
+    if (n) {
+      this.name = n;
+    }
   }
 
   greet(phrase: string) {
-    console.log(phrase + ' ' + this.name);
+    if (this.name) {
+      console.log(phrase + ' ' + this.name);
+    } else {
+      console.log('Hi !');
+    }
   }
 }
 
 let user1: Greetable;
 
-user1 = new Person('Mohammed');
+user1 = new Person();
 
 user1.greet('Hi there I am ');
 
